cd33c1fd642dd879e27c15e110cf8257ec0b8ed5
dingshaoyong <dingshaoyong@crc.com.hk>
2019-06-19 17:06:03 +0800

    修改应用创建逻辑,适配CI应用流水线. 修复feign调用设置问题. 修复异常逻辑.

--- src/main/java/io/choerodon/devops/domain/application/convertor/DevopsCIApplicationConvertor.java
+++ src/main/java/io/choerodon/devops/domain/application/convertor/DevopsCIApplicationConvertor.java
@@ -5,10 +5,10 @@ import io.choerodon.devops.domain.application.entity.CIApplicationE;
 import io.choerodon.devops.infra.dataobject.devopsCI.CIApplicationDO;
 import org.springframework.beans.BeanUtils;
 
-public class DevopsCIApplicationConvertor implements ConvertorI<CIApplicationE, Object, CIApplicationDO> {
+public class DevopsCIApplicationConvertor implements ConvertorI<CIApplicationE, CIApplicationDO, Object> {
 
     @Override
-    public CIApplicationE doToEntity(Object dataObject) {
+    public CIApplicationE doToEntity(CIApplicationDO dataObject) {
         CIApplicationE ciApplicationE = new CIApplicationE();
         BeanUtils.copyProperties(dataObject, ciApplicationE);
         return ciApplicationE;
