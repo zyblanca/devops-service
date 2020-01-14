0f0063b5c49d224b0a45b0cbd3ec439e2d5d220f
xiaxinyu3 <xiaxinyu3@crc.com.hk>
2020-01-14 17:00:25 +0800

    添加打印日志

--- src/main/java/io/choerodon/devops/app/service/impl/GitlabWebHookServiceImpl.java
+++ src/main/java/io/choerodon/devops/app/service/impl/GitlabWebHookServiceImpl.java
@@ -28,19 +28,20 @@ public class GitlabWebHookServiceImpl implements GitlabWebHookService {
     private SteamPipelineService pipelineService;
 
     public GitlabWebHookServiceImpl(DevopsMergeRequestRepository devopsMergeRequestRepository, DevopsGitService devopsGitService, DevopsGitlabCommitService devopsGitlabCommitService,
-                                    DevopsGitlabPipelineService devopsGitlabPipelineService, DevOpsCIService devOpsCIService,SteamPipelineService pipelineService) {
+                                    DevopsGitlabPipelineService devopsGitlabPipelineService, DevOpsCIService devOpsCIService, SteamPipelineService pipelineService) {
         this.devopsMergeRequestRepository = devopsMergeRequestRepository;
         this.devopsGitService = devopsGitService;
         this.devopsGitlabPipelineService = devopsGitlabPipelineService;
         this.devopsGitlabCommitService = devopsGitlabCommitService;
         this.devOpsCIService = devOpsCIService;
-        this.pipelineService=pipelineService;
+        this.pipelineService = pipelineService;
     }
 
     @Override
     public void forwardingEventToPortal(String body, String token) {
         JsonObject returnData = new JsonParser().parse(body).getAsJsonObject();
         String kind = returnData.get("object_kind").getAsString();
+        LOGGER.info("接收到Gitlab Web Hook，kind={}", kind);
         switch (kind) {
             case "merge_request":
                 DevopsMergeRequestDTO devopsMergeRequestDTO = JSONArray.parseObject(body, DevopsMergeRequestDTO.class, FastjsonParserConfigProvider.getParserConfig());
